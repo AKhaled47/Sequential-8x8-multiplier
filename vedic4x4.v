dc0f48473894d5103b978b2a8e4aa98f85979894
AKhaled47 <125220041+AKhaled47@users.noreply.github.com>
2023-07-12 20:12:10 +0300

    Update vedic4x4.v

--- vedic4x4.v
+++ vedic4x4.v
@@ -1,11 +1,12 @@
 module vedic4x4(
     input [3:0] a,b,
-    output [8:0] s
+    output [7:0] s
 );
 
 wire [3:0] v_out [3:0];
 wire [3:0] a_out [1:0];
 wire [1:0] c;
+wire final_carry;
 
 
 vedic2x2 u1 (a[1:0],b[1:0],v_out[0]);
@@ -15,9 +16,9 @@ vedic2x2 u4 (a[3:2],b[3:2],v_out[3]);
 
 FA_generic #(.N(4)) u5 ({2'b00,v_out[0][3:2]},v_out[1],1'b0,a_out[0],c[0]);
 
-FA_generic #(.N(4)) u6 (a_out[0],v_out[2],c[0],a_out[1],c[1]);
+FA_generic #(.N(4)) u6 (a_out[0],v_out[2],1'b0,a_out[1],c[1]);
 
-FA_generic #(.N(4)) u7 ({1'b0,c[1],a_out[1][3:2]},v_out[3],1'b0,s[7:4],s[8]);
+FA_generic #(.N(4)) u7 ({&c,^c,a_out[1][3:2]},v_out[3],1'b0,s[7:4],final_carry);
 
 assign s[1:0]=v_out[0][1:0];
 
